3baaa267b781bf85ee77d9484c4cc8d36ec78fde
Eduardo <edutraquino@gmail.com>
2022-01-03 21:18:41 -0300

    renaming app to calculator and add display test

--- __test__/app.test.ts
+++ __test__/app.test.ts
@@ -1,34 +1,45 @@
-import { App, Display } from "../script"
+import { Calculator, Display } from "../script"
 
 describe("Should receives the right value to operation with 4 basic signals (+, -, *, /)", () => {
 
     const input = document.createElement("input");
     const display = new Display(input);
-    const app = new App(display);
+    const calculator = new Calculator(display);
 
     it("Sum", () => {
-        const sum = app.preparedOperation("+", 5);
+        const sum = calculator.preparedOperation("+", 5);
         expect(sum(5)).toBe(10);
         expect(sum(50)).toBe(55);
     })
 
     it("Division", () => {
-        const minus = app.preparedOperation("-", 2);
+        const minus = calculator.preparedOperation("-", 2);
         expect(minus(3)).toBe(-1);
         expect(minus(1)).toBe(1);
     })
 
     it("Multiply", () => {
 
-        const multiply = app.preparedOperation("*", 2);
+        const multiply = calculator.preparedOperation("*", 2);
         expect(multiply(3)).toBe(6);
         expect(multiply(1)).toBe(2);
         expect(multiply(-5)).toBe(-10);
     })
 
     it("Division", () => {
-        const division = app.preparedOperation("/", 10);
+        const division = calculator.preparedOperation("/", 10);
         expect(division(2)).toBe(5);
         expect(division(1)).toBe(10);
     })
+})
+
+describe("Getting values from display", () => {
+
+    const input = document.createElement("input");
+    const display = new Display(input);
+    const getTextFn = jest.spyOn(display, "getText");
+    const calculator = new Calculator(display);
+
+    calculator.storeValueOrDoTheMath("+");
+    expect(getTextFn).toBeCalledTimes(1);
 })
