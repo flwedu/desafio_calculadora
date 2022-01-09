ca313e42bf6dd2f4315fb4b2ae77c53a0b9499da
Eduardo <edutraquino@gmail.com>
2022-01-09 17:34:29 -0300

    using it.each() to sum test methods

--- __test__/calculator.test.ts
+++ __test__/calculator.test.ts
@@ -4,19 +4,20 @@ describe("Should receives the right value to operation with 4 basic signals (+, 
 
     const calculator = new Calculator();
 
-    it("Sum", () => {
+    it.each(
+        [
+            [6, 4, "10"],
+            [40, -8, "32"],
+            [5, -80, "-75"]
+        ])("Sum", (valueA, valueB, expected) => {
 
-        expect(calculator.calculate({
-            valueA: 6,
-            valueB: 4,
-            signal: "+"
-        })).toBe("10");
-        expect(calculator.calculate({
-            valueA: 50,
-            valueB: 5,
-            signal: "+"
-        })).toBe("55");
-    })
+            const data = {
+                valueA,
+                valueB,
+                signal: "+"
+            }
+            expect(calculator.calculate(data)).toBe(expected);
+        })
 
     it("Division", () => {
         expect(calculator.calculate({
