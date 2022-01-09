1a249ccb48ad02c66c62b4615d65b5c781657423
Eduardo <edutraquino@gmail.com>
2022-01-09 09:42:02 -0300

    class contains only math tests

--- __test__/calculator.test.ts
+++ __test__/calculator.test.ts
@@ -1,113 +1,43 @@
 import Calculator from "../src/Calculator";
 import Display from "../src/Display";
+import { EventEmitter } from "../src/EventEmitter";
 
 describe("Should receives the right value to operation with 4 basic signals (+, -, *, /)", () => {
 
     const input = document.createElement("input");
     const display = new Display(input);
-    const calculator = new Calculator(display);
+    const eventEmitter = EventEmitter;
+    const calculator = new Calculator(display, eventEmitter);
+
+    beforeEach(() => {
+        calculator.clearStoredOperation();
+    })
 
     it("Sum", () => {
-        const sum = calculator.preparedOperation("+", 5);
-        expect(sum(5)).toBe(10);
-        expect(sum(50)).toBe(55);
+
+        expect(calculator.calculate(6, 4, "+")).toBe(10);
+        expect(calculator.calculate(50, 5, "+")).toBe(55);
     })
 
     it("Division", () => {
-        const minus = calculator.preparedOperation("-", 2);
-        expect(minus(3)).toBe(-1);
-        expect(minus(1)).toBe(1);
+        expect(calculator.calculate(5, 6, "-")).toBe(-1);
+        expect(calculator.calculate(6, 5, "-")).toBe(1);
     })
 
     it("Multiply", () => {
 
-        const multiply = calculator.preparedOperation("*", 2);
-        expect(multiply(3)).toBe(6);
-        expect(multiply(1)).toBe(2);
-        expect(multiply(-5)).toBe(-10);
+        expect(calculator.calculate(8, 5, "*")).toBe(40);
+        expect(calculator.calculate(10, 5, "*")).toBe(50);
+        expect(calculator.calculate(5, -5, "*")).toBe(-25);
     })
 
     it("Division", () => {
-        const division = calculator.preparedOperation("/", 10);
-        expect(division(2)).toBe(5);
-        expect(division(1)).toBe(10);
-    })
-})
-
-describe("Updating the display", () => {
-
-    const input = document.createElement("input");
-    const display = new Display(input);
-    const calculator = new Calculator(display);
-
-    beforeEach(() => {
-        display.clearText();
-        calculator.clearStoredOperation();
+        expect(calculator.calculate(10, 5, "/")).toBe(2);
+        expect(calculator.calculate(2, -1, "/")).toBe(-2);
+        expect(calculator.calculate(5, 2, "/")).toBe(2.5);
     })
 
-    it("Should update the display with results when using '=' signal", () => {
-
-        display.addText("10");
-        calculator.storeValueOrDoTheMath("+")
-        display.addText("15");
-        calculator.storeValueOrDoTheMath("=");
-
-        expect(display.getText()).toEqual("25");
-
-    });
-
-    it("Should get data from display and show the results", () => {
-
-        const getTextFn = jest.spyOn(display, "getText");
-
-        display.addText("45");
-        calculator.storeValueOrDoTheMath("-");
-        expect(getTextFn).toBeCalledTimes(1);
-
-        display.addText("6");
-        calculator.storeValueOrDoTheMath("*")
-        expect(getTextFn).toBeCalledTimes(2);
-
-        expect(display.getText()).toEqual("39");
-    })
-})
-
-describe("Other math operations with signals + and =", () => {
-
-    const input = document.createElement("input");
-    const display = new Display(input);
-    const calculator = new Calculator(display);
-
-    beforeEach(() => {
-        display.clearText();
-        calculator.clearStoredOperation();
-    })
-
-    it("repeating the value to sum when clicked in = then +", () => {
-
-        display.addText("3");
-        calculator.storeValueOrDoTheMath("+");
-        calculator.storeValueOrDoTheMath("=");
-
-        expect(display.getText()).toEqual("6");
-        calculator.storeValueOrDoTheMath("=");
-
-        expect(display.getText()).toEqual("9");
-    })
-
-    it("repeating the sum when clicked in '='", () => {
-
-        display.addText("5");
-
-        calculator.storeValueOrDoTheMath("=");
-        expect(display.getText()).toEqual("5");
-
-        calculator.storeValueOrDoTheMath("+");
-        display.addText("10");
-        calculator.storeValueOrDoTheMath("=");
-        expect(display.getText()).toEqual("15");
-
-        calculator.storeValueOrDoTheMath("=");
-        expect(display.getText()).toEqual("25");
+    it("division by 0 returns Infinity", () => {
+        expect(calculator.calculate(5, 0, "/")).toBe(Infinity);
     })
 })
