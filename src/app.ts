def671effffed81f1621c7ed6bf9c4124d6a4d37
Eduardo <edutraquino@gmail.com>
2022-01-09 11:34:31 -0300

    add event listners

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,58 @@
 import Calculator from "./Calculator";
+import { EventEmitter } from "./core/EventEmitter";
+import { IMathOperation } from "./core/MathOperation";
 import Display from "./Display";
 
 const displayHtmlElementRef = document.getElementById("display") as HTMLInputElement;
-const numberButtons: NodeListOf<HTMLInputElement> = document.querySelectorAll("input.number");
-const operatorButtons: NodeListOf<HTMLInputElement> = document.querySelectorAll("input.operator");
 
 const display = new Display(displayHtmlElementRef);
-const calculator = new Calculator(display);
+const calculator = new Calculator();
+
+let lastSignal = "";
+let valueA: number | null = null;
+let valueB: number | null = null;
 
 /**
- * Buttons event listners
+ * EventListners to listen in this class:
+ *  numberClicked
+ *  calculate
+ *  signalClicked
+ *  clear
  */
-numberButtons.forEach(button => {
-    button.onclick = () => {
-        display.addText(button.value);
-    }
+
+EventEmitter.on("numberClicked", (value: string) => {
+    display.addText(value);
 })
 
-operatorButtons.forEach(button => {
-    button.onclick = () => {
-        calculator.storeValueOrDoTheMath(button.value);
+EventEmitter.on("calculate", (data: IMathOperation) => {
+    const result = calculator.calculate(data);
+    display.setDisplay(result);
+    valueA = null;
+    valueB = null;
+})
+
+EventEmitter.on("signalClicked", (signal: string) => {
+
+    if (signal != "=")
+        lastSignal = signal;
+
+    if (!valueA)
+        valueA = Number(display.getText());
+    else
+        valueB = Number(display.getText());
+
+    const data: IMathOperation = {
+        valueA,
+        valueB,
+        signal: lastSignal
     }
+
+    if (signal = "=") {
+        EventEmitter.emit("calculate", data)
+    }
+
+})
+
+EventEmitter.on("clear", () => {
+    display.clearText();
 })
