ba90dcf015f5281ba0b9dac8a3e130f6a7aa74dd
Eduardo <edutraquino@gmail.com>
2022-01-09 11:56:23 -0300

    eventEmitter class refactoring

--- src/core/EventEmitter.ts
+++ src/core/EventEmitter.ts
@@ -1,24 +1,20 @@
-export interface IEventEmitter {
-    events: Map<string, Function[]>,
-    on: Function,
-    emit: Function
-}
-
-export const EventEmitter: IEventEmitter = {
-
+export const EventEmitter = {
     events: new Map<string, Function[]>(),
 
-    on: (eventName: string, fn: Function) => {
+    on: (topic: string, fn: Function) => {
+        let presentFns = EventEmitter.events.get(topic);
 
-        if (!EventEmitter.events.has(eventName)) {
-            EventEmitter.events.set(eventName, Array.of());
+        if (EventEmitter.events.has(topic)) {
+            //@ts-ignore
+            EventEmitter.events.set(topic, [...presentFns, fn]);
         }
-
-        EventEmitter.events.get(eventName)?.push(fn);
+        return EventEmitter.events.set(topic, [fn]);
     },
 
-    emit: (eventName: string, value: any) => {
-
-        EventEmitter.events.get(eventName)?.forEach(fn => fn(value));
-    }
-}
+    emit: (topic: string, data: any) => {
+        const listeners = EventEmitter.events.get(topic);
+        if (Array.isArray(listeners) && listeners.length) {
+            listeners.forEach((event) => event(data));
+        }
+    },
+};
