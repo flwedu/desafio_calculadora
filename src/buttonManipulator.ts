3b75e6164d8b64b7c7213aa461dcdbce0868d3a1
Eduardo <edutraquino@gmail.com>
2022-01-09 18:12:53 -0300

    add "button_clear"

--- src/buttonManipulator.ts
+++ src/buttonManipulator.ts
@@ -2,6 +2,7 @@ import { EventEmitter } from "./core/EventEmitter";
 
 const numberButtons: NodeListOf<HTMLInputElement> = document.querySelectorAll("input.number");
 const operatorButtons: NodeListOf<HTMLInputElement> = document.querySelectorAll("input.operator");
+const button__clear = document.getElementById("button_clear");
 
 /**
  * Buttons event listners
@@ -18,4 +19,8 @@ operatorButtons.forEach(button => {
     }
 })
 
-export { numberButtons, operatorButtons };
+button__clear?.addEventListener("click", () => {
+    EventEmitter.emit("clear", null)
+})
+
+export { numberButtons, operatorButtons, button__clear };
