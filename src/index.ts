8e84b8ad51bdaa98d791be2dca6d4571fd82bf5d
Eduardo Aquino <edutraquino@gmail.com>
2022-03-29 18:12:14 -0300

    refactor: app is now a class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-import "./Buttons";
 import "./style/main.scss";
-import "./app"
+import { App } from "./app";
+import { Calculator } from "./Calculator";
+import { EventEmitter } from "./core/EventEmitter";
+import { DisplayManager } from "./DisplayManager";
+
+const display = new DisplayManager(document.getElementById("display") as HTMLInputElement);
+const calculator = new Calculator();
+const app = new App(display, calculator, EventEmitter);
+app.initialize();
+
+import "./Buttons";
